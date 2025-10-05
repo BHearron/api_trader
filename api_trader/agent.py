bb877f8aa02b5785f1349a5e4dbaddefca539a73
Brock Hearron <behearron@gmail.com>
2025-10-05 11:28:44 -0400

    File Updated
    updated .gitignore. added function outlines to agent

--- api_trader/agent.py
+++ api_trader/agent.py
@@ -1,3 +1,6 @@
+# Standard library imports
+from datetime import datetime
+
 # Third party imports
 from dotenv import dotenv_values
 
@@ -6,7 +9,26 @@ DEFAULT_ENV_FILE = "../.env"
 
 
 class Agent:
-    def __init__(self, env_filepath: str=DEFAULT_ENV_FILE):
+    def __init__(self, env_filepath: str=DEFAULT_ENV_FILE) -> None:
         config = dotenv_values(env_filepath)
         self.api_key = config["api_key"]
         self.api_secret = config["secret"]
+
+        self.symbols = set()
+
+    def set_symbols(self, symbols: list) -> None:
+        self.symbols.update(symbols)
+
+    def _get_data(self, start: datetime, end: datetime) -> None:
+        pass
+
+    def get_data(self, measure: str, amount: int, start_date: datetime) -> None:
+        pass
+
+
+
+
+
+
+if __name__ == "__main__":
+    agent = Agent()
